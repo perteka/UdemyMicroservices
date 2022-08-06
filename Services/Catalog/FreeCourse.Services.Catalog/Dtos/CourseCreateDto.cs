dbc35b2a04d5ec121ab33f94793bd74c44398c7e
Ali Han Pertek <alihan.pertek@hotmail.com>
2022-08-06 10:28:56 +0300

    Add project files.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Dtos/CourseCreateDto.cs
@@ -0,0 +1,23 @@
+﻿namespace FreeCourse.Services.Catalog.Dtos
+{
+    public class CourseCreateDto
+    {
+        public string Name { get; set; }
+
+        public string Decription { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string picture { get; set; }
+
+
+        public string UserId { get; set; }
+
+        public FeatureDto Feature { get; set; }
+
+        public string CategoryId { get; set; }
+
+    }
+
+
+}
---
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCouse.Shared.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+    public interface ICategoryService
+    {
+        Task<Response<List<CategoryDto>>> GetAllAsync();
+        Task<Response<CategoryDto>> CreateAsync(CategoryDto categoryDto);
+        Task<Response<CategoryDto>> GetByIdAsync(string id);
+    }
+}
